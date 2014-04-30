eb81f59e4b3b006f0f2d3a41a1cf3b32c63b8783
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-04-30 22:27:32 +0200

    installation bash script

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 #apt-get update
 #apt-get upgrade -y
 apt-get install -y git puppet librarian-puppet 
+echo "Updating / Cloning /opt/puppet"
 if [ -d /opt/puppet ] ; then
 	cd /opt/puppet
 	git pull
+	librarian-puppet update --verbose
 else
 	git clone https://RAT@git.opitz-consulting.de/scm/~rat/public-puppet.git /opt/puppet
+	librarian-puppet install --verbose --clean
 fi
-cd /opt/puppet && librarian-puppet install
+echo "applying puppet"
 puppet apply --modulepath=/opt/puppet/modules/ /opt/puppet/manifests/nodes.pp
